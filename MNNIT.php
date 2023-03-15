98d7602d00bdd14068c5020130e0f2d19e9f778a
Himanshu Tripathi <123529078+Himanshuadv@users.noreply.github.com>
2023-03-15 22:37:53 +0530

    now the landing is user friendly.

--- MNNIT.php
+++ MNNIT.php
@@ -63,13 +63,13 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
 <div class="container mt-4">
 
 <div class="jumbotron">
-  <h1 class="display-4"><?php echo "Welcome ". $_SESSION['username']?>! Hello, world!</h1>
+  <h1 class="display-4"><?php echo "Welcome ". $_SESSION['username']?>!</h1>
   <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
   <hr class="my-4">
   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <p class="lead">
+<!--   <p class="lead">
     <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
+  </p> -->
 </div>
 <h3><?php echo "Welcome ". $_SESSION['username']?>! You can now use this webssite</h3>
 <hr>
