4a421f3798d3de5b17f8661dd6d86a94412473d4
Himanshu Tripathi <123529078+Himanshuadv@users.noreply.github.com>
2023-03-16 21:55:59 +0530

    updated MNNIT.php

--- MNNIT.php
+++ MNNIT.php
@@ -22,7 +22,16 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <title>MNNIT HUB</title>
+    <style>
+
+      #profile{
+        width: 60px;
+        border-radius:10000px;
+      }
+      </style>
+
+
+    <title>MNNIT HUB!</title>
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -65,13 +74,13 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
 
 <div class="jumbotron">
   <h1 class="display-4"><?php echo "Welcome ". $_SESSION['username']?>!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <p class="lead">Now you can seamlessly interact with your college mates and know about your college and communities.</p>
   <hr class="my-4">
   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-<!--   <p class="lead">
+  <!-- <p class="lead">
     <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
   </p> -->
-    <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+  <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
   MY Profile
 </a>
 <!-- <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
@@ -80,13 +89,14 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
 
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasExampleLabel"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
-</svg><?php echo  $_SESSION['username']?>  </h5>
+    <!-- <h5 class="offcanvas-title" id="offcanvasExampleLabel"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/> -->
+ <!-- </svg> -->
+ <img src="image/me_and_dr.jpg" alt="Profile Picture" id="profile"><h2><?php echo  $_SESSION['username']?></h2>  </h5> 
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
-    <div>
+    <!-- <div>
       Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
     </div>
     <div class="dropdown mt-3">
@@ -96,14 +106,42 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
       <ul class="dropdown-menu">
         <li><a class="dropdown-item" href="#">Action</a></li>
         <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li> -->
+        <section>
+			<!-- <img src="me_and_dr.jpg" alt="Profile Picture" id="profile"> -->
+			<!-- <h2>Himanshu</h2> -->
+			<p> <h5> EMAIL: </h5> mnnit@gmail.com  </p>
+			<p>Location: PRAYAGRAJ,UP</p>
+			<p>About Me: I'm a student persuing BTECH in CSE at MNNIT ALLLAHABAD.</p>
+		</section>
+
+		<!-- Skills section -->
+		<section>
+			<h2>Skills</h2>
+			<ul>
+				<li>HTML</li>
+				<li>CSS</li>
+				<li>JavaScript</li>
+				<li>JAVA</li>
+			</ul>
+		</section>
+
+		<!-- Projects section -->
+		<section>
+			<h2>Projects</h2>
+			<ul>
+				<li><a href="#">Commmunity 1</a></li>
+				<li><a href="#">Community 2</a></li>
+				<li><a href="#">Community 3</a></li>
+			</ul>
+		</section>
+     
       </ul>
     </div>
   </div>
 </div>
+  
 </div>
-    
-    
 <h3><?php echo "Welcome ". $_SESSION['username']?>! You can now use this webssite</h3>
 <hr>
 
