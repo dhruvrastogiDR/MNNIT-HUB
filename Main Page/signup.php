c619d27e8e3e7d0cf7a679fc411a432a6424472c
dhruvrastogiDR <125766281+dhruvrastogiDR@users.noreply.github.com>
2023-03-19 22:50:58 +0530

    Update signup.php

--- Main Page/signup.php
+++ Main Page/signup.php
@@ -168,7 +168,7 @@ z-index: -1;
 <div class="collapse navbar-collapse" id="navbarNavDropdown">
 <ul class="navbar-nav">
   <li class="nav-item active">
-    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    <a class="nav-link" href="mhlandingpage.php">Home <span class="sr-only">(current)</span></a>
   </li>
  
   <li class="nav-item">
