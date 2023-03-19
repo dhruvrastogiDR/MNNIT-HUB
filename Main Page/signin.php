10326fa1210c086653562a55f7be89900ad2793b
dhruvrastogiDR <125766281+dhruvrastogiDR@users.noreply.github.com>
2023-03-19 22:49:46 +0530

    Update signin.php

--- Main Page/signin.php
+++ Main Page/signin.php
@@ -145,7 +145,7 @@ if(empty($err))
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
   <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="mhlandingpage.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="signup.php">Sign-Up</a>
